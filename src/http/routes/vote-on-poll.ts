07d27a5014decfcbf3a7775f936dd3968bb2a6a9
Leonardo Silva <silvaleonardo.ti@gmail.com>
2024-02-08 23:50:57 -0300

    commit aula 2

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+import { prisma } from '../../lib/prisma';
+import { FastifyInstance } from 'fastify';
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post('/polls/:pollId/votes', async (request, reply) => {
+    const voteOnPoll = z.object({
+      pollOptionId: z.string().uuid(),
+    });
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid(),
+    });
+
+    const { pollId } = voteOnPollParams.parse(request.params);
+    const { pollOptionId } = voteOnPoll.parse(request.body);
+
+    return reply.status(201).send();
+  });
+}

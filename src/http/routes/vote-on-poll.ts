a50cb0c676d3d7db647478cd454d6cd1885a4fbe
Leonardo Silva <silvaleonardo.ti@gmail.com>
2024-02-09 11:54:04 -0300

    feat: add sessionId cookie validation and creating new vote feature

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { prisma } from '../../lib/prisma';
+import { randomUUID } from 'node:crypto';
 import { FastifyInstance } from 'fastify';
 
 export async function voteOnPoll(app: FastifyInstance) {
@@ -13,6 +14,48 @@ export async function voteOnPoll(app: FastifyInstance) {
 
     const { pollId } = voteOnPollParams.parse(request.params);
     const { pollOptionId } = voteOnPoll.parse(request.body);
+
+    let { sessionId } = request.cookies;
+
+    if (sessionId) {
+      const userPreviousVoteOnThisPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId,
+          },
+        },
+      });
+
+      if (userPreviousVoteOnThisPoll && userPreviousVoteOnThisPoll.pollOptionId !== pollOptionId) {
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnThisPoll.id,
+          },
+        });
+      } else if (userPreviousVoteOnThisPoll) {
+        return reply.status(400).send({ message: 'You already voted on this poll.' });
+      }
+    }
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        signed: true,
+        httpOnly: true,
+      });
+    }
+
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId,
+      },
+    });
 
     return reply.status(201).send();
   });
